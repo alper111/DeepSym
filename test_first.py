fc7965352deeffc8d8d60baa1197ee321ffa08c5
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2020-04-28 13:30:47 +0300

    test first for new data

--- test_first.py
+++ test_first.py
@@ -11,15 +11,12 @@ args = parser.parse_args()
 trainset = data.FirstLevelDataset()
 codes = torch.load(os.path.join(args.ckpt, "codes_first.torch"))
 
-
 fig, ax = plt.subplots(5, 10, figsize=(18, 10))
-unnormalized = (trainset.objects.reshape(-1, 128*128) * (trainset.obj_std + 1e-6) + trainset.obj_mu)
-unnormalized = unnormalized.reshape(-1, 1, 128, 128)
 
 for i in range(5):
     for j in range(10):
         idx = i * 10 + j
-        ax[i, j].imshow(unnormalized[idx, 0])
+        ax[i, j].imshow(trainset.objects[idx, 12])
         ax[i, j].axis("off")
         ax[i, j].set_title(codes[idx].numpy())
 plt.show()
