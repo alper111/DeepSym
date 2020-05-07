133cce810262014a35cdeee80a5d2993f1f7cd2f
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2020-05-07 16:01:37 +0300

    bug fix

--- test_first.py
+++ test_first.py
@@ -29,7 +29,7 @@ fig, ax = plt.subplots(5, 10, figsize=(12, 7))
 for i in range(5):
     for j in range(10):
         idx = i * 10 + j
-        ax[i, j].imshow(trainset.objects[idx, 12]*0.0094+0.279)
+        ax[i, j].imshow(objects[idx, 12]*0.0094+0.279)
         ax[i, j].axis("off")
         ax[i, j].set_title(codes[idx].numpy())
 plt.show()
