67761a80d504343494744ce4ecfef3a56628ece9
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2020-05-07 21:37:00 +0300

    add second level affordances

--- models.py
+++ models.py
@@ -8,72 +8,103 @@ class AffordanceModel:
 
     def __init__(self, opts):
         self.device = torch.device(opts["device"])
-        self.encoder = build_encoder(opts).to(self.device)
-        self.decoder = MLP([opts["code_dim"] + 3] + [opts["hidden_dim"]] * opts["depth"] + [3]).to(self.device)
+        self.encoder1 = build_encoder(opts, 1).to(self.device)
+        self.encoder2 = build_encoder(opts, 2).to(self.device)
+        self.decoder1 = MLP([opts["code1_dim"] + 3] + [opts["hidden_dim"]] * opts["depth"] + [3]).to(self.device)
+        self.decoder2 = MLP([opts["code2_dim"] + opts["code1_dim"]*2] + [opts["hidden_dim"]] * opts["depth"] + [6]).to(self.device)
         self.optimizer = torch.optim.Adam(lr=opts["learning_rate"],
                                           params=[
-                                              {"params": self.encoder.parameters()},
-                                              {"params": self.decoder.parameters()}],
+                                              {"params": self.encoder1.parameters()},
+                                              {"params": self.encoder2.parameters()},
+                                              {"params": self.decoder1.parameters()},
+                                              {"params": self.decoder2.parameters()}],
                                           amsgrad=True)
-        self.criterion = torch.nn.MSELoss(reduction="mean")
+        self.criterion = torch.nn.MSELoss()
         self.iteration = 0
         self.save_path = opts["save"]
 
-    def loss(self, state, action, effect):
-        h = self.encoder(state)
-        aug = torch.eye(3, device=self.device)[action]
-        h_aug = torch.cat([h, aug], dim=-1)
-        effect_pred = self.decoder(h_aug)
-        loss = self.criterion(effect_pred, effect)
+    def loss1(self, sample):
+        h = self.encoder1(sample["object"].to(self.device))
+        action = torch.eye(3, device=self.device)[sample["action"]]
+        h_aug = torch.cat([h, action], dim=-1)
+        effect_pred = self.decoder1(h_aug)
+        loss = self.criterion(effect_pred, sample["effect"].to(self.device))
         return loss
 
-    def one_pass_optimize(self, loader):
+    def loss2(self, sample):
+        obj = sample["object"].to(self.device)
+        with torch.no_grad():
+            h1 = self.encoder1(obj.reshape(-1, 1, obj.shape[2], obj.shape[3]))
+        h1 = h1.reshape(obj.shape[0], -1)
+        h2 = self.encoder2(obj)
+        h_aug = torch.cat([h1, h2], dim=-1)
+        effect_pred = self.decoder2(h_aug)
+        loss = self.criterion(effect_pred, sample["effect"].to(self.device))
+        return loss
+
+    def one_pass_optimize(self, loader, level):
         running_avg_loss = 0.0
         for i, sample in enumerate(loader):
             self.optimizer.zero_grad()
-            state = sample["object"].to(self.device)
-            action = sample["action"]
-            effect = sample["effect"].to(self.device)
-            loss = self.loss(state, action, effect)
+            if level == 1:
+                loss = self.loss1(sample)
+            else:
+                loss = self.loss2(sample)
             loss.backward()
             running_avg_loss += loss.item()
             self.iteration += 1
             self.optimizer.step()
         return running_avg_loss/i
 
-    def train(self, epoch, loader):
+    def train(self, epoch, loader, level):
         best_loss = 1e100
         for e in range(epoch):
-            epoch_loss = self.one_pass_optimize(loader)
+            epoch_loss = self.one_pass_optimize(loader, level)
             if epoch_loss < best_loss:
                 best_loss = epoch_loss
-                self.save(self.save_path, "_best")
+                self.save(self.save_path, "_best", level)
             if (e+1) % 100 == 0:
                 print("Epoch: %d, iter: %d, loss: %.4f" % (e+1, self.iteration, epoch_loss))
-                self.save(self.save_path, "_last")
+                self.save(self.save_path, "_last", level)
 
-    def load(self, path, ext):
-        encoder_dict = torch.load(os.path.join(path, "encoder_first"+ext+".ckpt"))
-        decoder_dict = torch.load(os.path.join(path, "decoder_first"+ext+".ckpt"))
-        self.encoder.load_state_dict(encoder_dict)
-        self.decoder.load_state_dict(decoder_dict)
+    def load(self, path, ext, level):
+        if level == 1:
+            encoder = self.encoder1
+            decoder = self.decoder1
+        else:
+            encoder = self.encoder2
+            decoder = self.decoder2
 
-    def save(self, path, ext):
-        encoder_dict = self.encoder.eval().cpu().state_dict()
-        decoder_dict = self.decoder.eval().cpu().state_dict()
-        torch.save(encoder_dict, os.path.join(path, "encoder_first"+ext+".ckpt"))
-        torch.save(decoder_dict, os.path.join(path, "decoder_first"+ext+".ckpt"))
-        self.encoder.train().to(self.device)
-        self.decoder.train().to(self.device)
+        encoder_dict = torch.load(os.path.join(path, "encoder"+str(level)+ext+".ckpt"))
+        decoder_dict = torch.load(os.path.join(path, "decoder"+str(level)+ext+".ckpt"))
+        encoder.load_state_dict(encoder_dict)
+        decoder.load_state_dict(decoder_dict)
 
-    def print_model(self):
+    def save(self, path, ext, level):
+        if level == 1:
+            encoder = self.encoder1
+            decoder = self.decoder1
+        else:
+            encoder = self.encoder2
+            decoder = self.decoder2
+
+        encoder_dict = encoder.eval().cpu().state_dict()
+        decoder_dict = decoder.eval().cpu().state_dict()
+        torch.save(encoder_dict, os.path.join(path, "encoder"+str(level)+ext+".ckpt"))
+        torch.save(decoder_dict, os.path.join(path, "decoder"+str(level)+ext+".ckpt"))
+        encoder.train().to(self.device)
+        decoder.train().to(self.device)
+
+    def print_model(self, level):
+        encoder = self.encoder1 if level == 1 else self.encoder2
+        decoder = self.decoder1 if level == 1 else self.decoder2
         print("="*10+"ENCODER"+"="*10)
-        print(self.encoder)
-        print("parameter count: %d" % utils.get_parameter_count(self.encoder))
+        print(encoder)
+        print("parameter count: %d" % utils.get_parameter_count(encoder))
         print("="*27)
         print("="*10+"DECODER"+"="*10)
-        print(self.decoder)
-        print("parameter count: %d" % utils.get_parameter_count(self.decoder))
+        print(decoder)
+        print("parameter count: %d" % utils.get_parameter_count(decoder))
         print("="*27)
 
 
@@ -197,7 +228,11 @@ class Avg(torch.nn.Module):
         return "dims=[" + ", ".join(list(map(str, self.dims))) + "]"
 
 
-def build_encoder(opts):
+def build_encoder(opts, level):
+    if level == 1:
+        code_dim = opts["code1_dim"]
+    else:
+        code_dim = opts["code2_dim"]
     if opts["cnn"]:
         L = len(opts["filters"])-1
         stride = 2
@@ -208,13 +243,13 @@ def build_encoder(opts):
             encoder.append(ConvBlock(in_channels=opts["filters"][i], out_channels=opts["filters"][i+1],
                                      kernel_size=4, stride=stride, padding=1, batch_norm=opts["batch_norm"]))
         encoder.append(Avg([2, 3]))
-        # encoder.append(MLP([lat, opts["code_dim"]]))
-        encoder.append(MLP([opts["filters"][-1], opts["code_dim"]]))
+        # encoder.append(MLP([lat, code_dim]))
+        encoder.append(MLP([opts["filters"][-1], code_dim]))
         encoder.append(STLayer())
     else:
         encoder = [
             Flatten([1, 2, 3]),
-            MLP([[opts["size"]**2] + [opts["hidden_dim"]]*opts["depth"] + [opts["code_dim"]]],
+            MLP([[opts["size"]**2] + [opts["hidden_dim"]]*opts["depth"] + [code_dim]],
                 batch_norm=opts["batch_norm"]),
             STLayer()]
 
