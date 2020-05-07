e281105d1155ee0bd63126f2107ad2613f23f82c
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2020-05-07 21:37:46 +0300

    updated test

--- test_first.py
+++ test_first.py
@@ -15,15 +15,15 @@ opts = yaml.safe_load(open(file_loc, "r"))
 opts["device"] = "cpu"
 
 model = models.AffordanceModel(opts)
-model.load(args.ckpt, "_best")
+model.load(args.ckpt, "_best", 1)
 
 transform = data.default_transform(size=opts["size"], affine=False, mean=0.279, std=0.0094)
 trainset = data.FirstLevelDataset(transform=transform)
 loader = torch.utils.data.DataLoader(trainset, batch_size=150, shuffle=False)
 objects = iter(loader).next()["object"]
-model.encoder.eval()
+model.encoder1.eval()
 with torch.no_grad():
-    codes = model.encoder(objects)
+    codes = model.encoder1(objects)
 
 fig, ax = plt.subplots(5, 10, figsize=(12, 7))
 for i in range(5):
