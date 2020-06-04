320a84adcb83e283d2335d411944043e94967a70
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2020-06-04 22:10:20 +0300

    refactor

--- learn_rules.py
+++ learn_rules.py
@@ -33,6 +33,10 @@ for i in range(2**CODE_DIM):
     category = utils.decimal_to_binary(i, length=CODE_DIM)
     obj_names[category] = "objtype{}".format(i)
 
+file_loc = os.path.join(args.ckpt, "domain.pddl")
+if os.path.exists(file_loc):
+    os.remove(file_loc)
+
 pddl_code = utils.tree_to_code(tree, ["f%d" % i for i in range(K)], effect_names, obj_names)
 pretext = "(define (domain stack)\n"
 pretext += "\t(:requirements :typing :negative-preconditions :probabilistic-effects :conditional-effects)\n"
@@ -40,28 +44,29 @@ pretext += "\t(:predicates"
 
 for i in range(K):
     pretext += "\n\t\t(%s) " % effect_names[i]
-pretext += "\n\t\t(pickloc ?x)\n\t\t(instack ?x)\n\t\t(stackloc ?x)\n\t\t(larger ?x ?y)"
-pretext += "\n\t\t(hollow ?x)\n\t\t(stable ?x)\n\t\t(sphere ?x)\n\t\t(cylinder ?x)"
+pretext += "\n\t\t(pickloc ?x)\n\t\t(instack ?x)\n\t\t(stackloc ?x)\n\t\t(relation0 ?x ?y)"
+for i in range(2**CODE_DIM):
+    pretext += "\n\t\t(" + obj_names[utils.decimal_to_binary(i, length=CODE_DIM)] + " ?x)"
 for i in range(7):
     pretext += "\n\t\t(H%d)" % i
 for i in range(7):
     pretext += "\n\t\t(S%d)" % i
 pretext += "\n\t)"
-print(pretext, file=open(os.path.join(args.ckpt, "domain.pddl"), "a"))
+print(pretext, file=file_loc, "a"))
 
 action_template = "\t(:action act%d\n\t\t:parameters (?above ?below)"
 for i, (precond, effect) in enumerate(pddl_code):
-    print(action_template % i, file=open(os.path.join(args.ckpt, "domain.pddl"), "a"))
-    print("\t\t"+precond, file=open(os.path.join(args.ckpt, "domain.pddl"), "a"))
-    print("\t\t"+effect, file=open(os.path.join(args.ckpt, "domain.pddl"), "a"))
-    print("\t)", file=open(os.path.join(args.ckpt, "domain.pddl"), "a"))
+    print(action_template % i, file=file_loc, "a"))
+    print("\t\t"+precond, file=file_loc, "a"))
+    print("\t\t"+effect, file=file_loc, "a"))
+    print("\t)", file=file_loc, "a"))
 for i in range(6):
-    print("\t(:action gainheight%d" % (i+1), file=open(os.path.join(args.ckpt, "domain.pddl"), "a"))
-    print("\t\t:precondition (and (stacked) (H%d))" % i, file=open(os.path.join(args.ckpt, "domain.pddl"), "a"))
-    print("\t\t:effect (and (not (H%d)) (H%d) (not (stacked)))\n\t)" % (i, i+1), file=open(os.path.join(args.ckpt, "domain.pddl"), "a"))
+    print("\t(:action gainheight%d" % (i+1), file=file_loc, "a"))
+    print("\t\t:precondition (and (stacked) (H%d))" % i, file=file_loc, "a"))
+    print("\t\t:effect (and (not (H%d)) (H%d) (not (stacked)))\n\t)" % (i, i+1), file=file_loc, "a"))
 for i in range(6):
-    print("\t(:action gainstack%d" % (i+1), file=open(os.path.join(args.ckpt, "domain.pddl"), "a"))
-    print("\t\t:precondition (and (inserted) (S%d))" % i, file=open(os.path.join(args.ckpt, "domain.pddl"), "a"))
-    print("\t\t:effect (and (not (S%d)) (S%d) (not (inserted)))\n\t)" % (i, i+1), file=open(os.path.join(args.ckpt, "domain.pddl"), "a"))
+    print("\t(:action gainstack%d" % (i+1), file=file_loc, "a"))
+    print("\t\t:precondition (and (inserted) (S%d))" % i, file=file_loc, "a"))
+    print("\t\t:effect (and (not (S%d)) (S%d) (not (inserted)))\n\t)" % (i, i+1), file=file_loc, "a"))
 
 print(")", file=open(os.path.join(args.ckpt, "domain.pddl"), "a"))
