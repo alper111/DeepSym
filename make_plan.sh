49ac9439fdbd75202c1f775e0b47dce18c6258e6
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2020-06-08 23:07:25 +0300

    remove bash issues

--- make_plan.sh
+++ make_plan.sh
@@ -1,28 +1,29 @@
+#!/bin/bash
 # get save location
-loc="$(cat $1 | grep save: | sed 's/^.*: //')"
+loc="$(grep save: $1 | sed 's/^.*: //')"
 
 # transform image to pddl problem
 python recognize.py -opts $1
 # concatenate domain and problem file
-cat $loc/"domain.pddl" > $loc/"temp.pddl"
-cat $loc/"problem.pddl" >> $loc/"temp.pddl"
+cat "$loc/domain.pddl" > "$loc/temp.pddl"
+cat "$loc/problem.pddl" >> "$loc/temp.pddl"
 # open PDDL server
-mdpsim --port=2322 -R 100 --time-limit=10000 $loc/"temp.pddl" & _pid=$!
+mdpsim --port=2322 -R 100 --time-limit=10000 "$loc/temp.pddl" & _pid=$!
 # save mdpsim pid
 echo "$_pid" > server.pid
 # run planner and save temporary result to planresult.txt
 # to see other planning options, run the planner without any argument
 # e.g. ./mini-gpt/planner
-./mini-gpt/planner -v 100 -h ff localhost:2322 $loc/"temp.pddl" dom1 > $loc/"planresult.txt"
+./mini-gpt/planner -v 100 -h ff localhost:2322 "$loc/temp.pddl" dom1 > "$loc/planresult.txt"
 # kill mdpsim server
-kill -9 $(cat server.pid)
+kill -9 "$(cat server.pid)"
 # remove auxilliary files
 rm server.pid
 rm -r logs
 # parse the plan result
 # see the plan in {savepath}/plan.txt
-python parse_plan.py -opts $1
-cat $loc/"plan.txt" >> $loc/"objects.txt"
-rm $loc/"planresult.txt"
-rm $loc/"plan.txt"
-mv $loc/"objects.txt" $loc/"plan.txt"
+python parse_plan.py -opts "$1"
+cat "$loc/plan.txt" >> "$loc/objects.txt"
+rm "$loc/planresult.txt"
+rm "$loc/plan.txt"
+mv "$loc/objects.txt" "$loc/plan.txt"
--- train_run.sh
+++ train_run.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 # These will be run only once.
 
 # train effect prediction model
-python train.py -opts $1
+python train.py -opts "$1"
 # cluster effect space
-python cluster.py -opts $1
+python cluster.py -opts "$1"
 # save object categories
-python save_cat.py -opts $1
+python save_cat.py -opts "$1"
 # transform the learned prediction model into PDDL
 # code by learning decision tree and encoding
 # effect probabilities.
-python learn_rules.py -opts $1
+python learn_rules.py -opts "$1"
