fecdba6b022c18e92388ee1c84c8379c4ae8acb0
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2020-06-08 22:58:14 +0300

    training script

---
+++ train_run.sh
@@ -0,0 +1,12 @@
+# These will be run only once.
+
+# train effect prediction model
+python train.py -opts $1
+# cluster effect space
+python cluster.py -opts $1
+# save object categories
+python save_cat.py -opts $1
+# transform the learned prediction model into PDDL
+# code by learning decision tree and encoding
+# effect probabilities.
+python learn_rules.py -opts $1
