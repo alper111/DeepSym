17cf8b022b7ac95288e790aff823a2abe788815d
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2020-06-08 22:57:54 +0300

    add additional hyperparam to yaml

--- train.py
+++ train.py
@@ -1,12 +1,16 @@
 import os
+import argparse
 import time
 import yaml
 import torch
 import models
 import data
 
+parser = argparse.ArgumentParser("Train effect prediction models.")
+parser.add_argument("-opts", help="option file", type=str, required=True)
+args = parser.parse_args()
 
-opts = yaml.safe_load(open("opts.yaml", "r"))
+opts = yaml.safe_load(open(args.opts, "r"))
 if not os.path.exists(opts["save"]):
     os.makedirs(opts["save"])
 opts["time"] = time.asctime(time.localtime(time.time()))
@@ -20,25 +24,21 @@ device = torch.device(opts["device"])
 # load the first level data
 transform = data.default_transform(size=opts["size"], affine=True, mean=0.279, std=0.0094)
 trainset = data.FirstLevelDataset(transform=transform)
-loader = torch.utils.data.DataLoader(trainset, batch_size=opts["batch_size"], shuffle=True)
+loader = torch.utils.data.DataLoader(trainset, batch_size=opts["batch_size1"], shuffle=True)
 
 model = models.AffordanceModel(opts)
 if opts["load"] is not None:
     model.load(opts["load"], ext="", level=1)
     model.load(opts["load"], ext="", level=2)
 model.print_model(1)
-model.train(opts["epoch"], loader, 1)
+model.train(opts["epoch1"], loader, 1)
 
 # load the best encoder1
 model.load(opts["save"], "_best", 1)
 
-# change hyperparams for the second level
-opts["batch_size"] = 50
-opts["epoch"] = 200
-
 # load the second level data
 transform = data.default_transform(size=opts["size"], affine=True, mean=0.279, std=0.0094)
 trainset = data.SecondLevelDataset(transform=transform)
-loader = torch.utils.data.DataLoader(trainset, batch_size=opts["batch_size"], shuffle=True)
+loader = torch.utils.data.DataLoader(trainset, batch_size=opts["batch_size2"], shuffle=True)
 model.print_model(2)
-model.train(opts["epoch"], loader, 2)
+model.train(opts["epoch2"], loader, 2)
