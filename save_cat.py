bfaed86c312b55c84a15222ef8cf32f4f9118526
alper111 <ahmetoglu.alper@gmail.com>
2020-08-27 23:57:04 +0300

    save categories with centered objects

--- save_cat.py
+++ save_cat.py
@@ -11,6 +11,7 @@ parser.add_argument("-opts", help="option file", type=str, required=True)
 args = parser.parse_args()
 
 opts = yaml.safe_load(open(args.opts, "r"))
+opts["device"] = "cpu"
 device = torch.device(opts["device"])
 
 model = EffectRegressorMLP(opts)
@@ -21,29 +22,23 @@ model.encoder2.eval()
 
 transform = data.default_transform(size=opts["size"], affine=False, mean=0.279, std=0.0094)
 relations = torch.load("data/relations.pt")
-X = torch.load("data/objectsZ.pt")
-B, C, H, W = X.shape
-Y = torch.empty(B*C, 1, opts["size"], opts["size"])
-X = X.reshape(B*C, 1, H, W)
+X = torch.load("data/objectsZ.pt")[:, 12:13]
+B, _, H, W = X.shape
+Y = torch.empty(B, 1, opts["size"], opts["size"])
 
-for i in range(B*C):
+for i in range(B):
     Y[i] = transform(X[i])
 
 with torch.no_grad():
     category1 = model.encoder1(Y.to(device))
 category1 = category1.int()
-category2 = torch.empty((B*C)**2, 1, device=device, dtype=torch.int)
-# we cannot batch process this.
-for i in range(B*C):
-    y_i = Y[i].repeat(B*C, 1, 1, 1)
-    concat = torch.cat([y_i, Y], dim=1)
-    with torch.no_grad():
-        category2[i*B*C:(i+1)*B*C] = model.encoder2(concat.to(device))
-    if (i+1) % 50 == 0:
-        print("%d/%d" % (i+1, B*C))
 
+left_img = Y.repeat_interleave(B, 0)
+right_img = Y.repeat(B, 1, 1, 1)
+concat = torch.cat([left_img, right_img], dim=1)
 
-left = category1.repeat_interleave(B*C, 0)
-right = category1.repeat(B*C, 1)
-category_all = torch.cat([left, right, category2], dim=-1)
+category2 = model.encoder2(concat.to(device)).int()
+left_cat = category1.repeat_interleave(B, 0)
+right_cat = category1.repeat(B, 1)
+category_all = torch.cat([left_cat, right_cat, category2], dim=-1)
 torch.save(category_all.cpu(), os.path.join(opts["save"], "category.pt"))
